30037718667d3ab03094b3f080f44b8517f3d7b5
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-11-28 09:13:56 +0000

    First commit of skeleton project structure and husky hook
    This is the initital environment and husky hook for running lint on commit.

---
+++ src/day_00/index.ts
@@ -0,0 +1,4 @@
+function sum(a: bigint, b: bigint): bigint {
+  return a + b;
+}
+module.exports = sum;
---
+++ test/day_00/index.test.ts
@@ -0,0 +1,5 @@
+const t_sum = require('../../src/day_00/index.ts');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(t_sum(1, 2)).toBe(3);
+});
