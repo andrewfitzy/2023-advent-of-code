be73c45b332b92bc5d7689da165862e134f34ecc
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-02 06:32:55 +0000

    Fixing the first test to use ES6 module structure

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/test'],
-  testMatch: ['**/test/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testMatch: ['**/test/**/*test.ts'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
--- test/day_00/index.test.ts
+++ test/day_00/index.test.ts
@@ -1,5 +1,5 @@
-const t_sum = require('../../src/day_00/index.ts');
+import {sum} from '../../src/day_00/index';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(t_sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
