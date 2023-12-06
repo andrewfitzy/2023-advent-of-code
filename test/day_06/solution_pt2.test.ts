3277e546d73201885870dea1658ea8216eedf4ae
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-06 13:37:21 +0000

    Adding day 6 pt 2

--- test/day_06/solution_pt2.test.ts
+++ test/day_06/solution_pt2.test.ts
@@ -2,9 +2,17 @@ import {readFileAsStringArray} from '../utils/read_file';
 import {solve} from '../../src/day_06/solution_pt2';
 
 describe('test solve', () => {
-  test('puzzle input expects something', () => {
+  test('example input expects 71503', () => {
+    const input: Array<string> = [
+      'Time:      7  15   30',
+      'Distance:  9  40  200',
+    ];
+    expect(solve(input)).toBe(71503);
+  });
+
+  test('puzzle input expects 27102791', () => {
     const fileInput = `${__dirname}/puzzle_input.txt`;
     const input: Array<string> = readFileAsStringArray(fileInput);
-    expect(solve(input)).toBe(0);
+    expect(solve(input)).toBe(27102791);
   });
 });
