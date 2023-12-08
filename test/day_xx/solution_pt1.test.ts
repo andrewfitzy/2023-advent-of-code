82800447cdcd38d6a11a32f80ce7ae20321f945d
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-08 06:42:36 +0000

    adding day 8 pt 1

--- test/day_xx/solution_pt1.test.ts
+++ test/day_xx/solution_pt1.test.ts
@@ -2,7 +2,12 @@ import {readFileAsStringArray} from '../utils/read_file';
 import {solve} from '../../src/day_xx/solution_pt1';
 
 describe('test solve', () => {
-  test('puzzle input expects something', () => {
+  test('example input expects 0', () => {
+    const input: Array<string> = [];
+    expect(solve(input)).toBe(0);
+  });
+
+  xtest('puzzle input expects something', () => {
     const fileInput = `${__dirname}/puzzle_input.txt`;
     const input: Array<string> = readFileAsStringArray(fileInput);
     expect(solve(input)).toBe(0);
