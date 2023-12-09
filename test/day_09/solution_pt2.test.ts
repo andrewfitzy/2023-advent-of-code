8c2c61c051ca54e45661b72dac98b431610cd2da
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-09 08:37:40 +0000

    correcting naming

--- test/day_09/solution_pt2.test.ts
+++ test/day_09/solution_pt2.test.ts
@@ -1,18 +1,18 @@
 import {readFileAsStringArray} from '../test_utils/read_file';
-import {solve, getPreviousNumber} from '../../src/day_09/solution_pt2';
+import {solve, getNextNumber} from '../../src/day_09/solution_pt2';
 
 describe('test getNextNumber', () => {
   test('"0 3 6 9 12 15" expects -3', () => {
     const input: number[] = [0, 3, 6, 9, 12, 15];
-    expect(getPreviousNumber(input.reverse())).toBe(-3);
+    expect(getNextNumber(input.reverse())).toBe(-3);
   });
   test('"1 3 6 10 15 21" expects 0', () => {
     const input: number[] = [1, 3, 6, 10, 15, 21];
-    expect(getPreviousNumber(input.reverse())).toBe(0);
+    expect(getNextNumber(input.reverse())).toBe(0);
   });
   test('"10 13 16 21 30 45" expects 5', () => {
     const input: number[] = [10, 13, 16, 21, 30, 45];
-    expect(getPreviousNumber(input.reverse())).toBe(5);
+    expect(getNextNumber(input.reverse())).toBe(5);
   });
 });
 
