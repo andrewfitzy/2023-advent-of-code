cfe4a2772e38bcce6024a80d0725a0b3427a4b9e
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-12 20:10:42 +0000

    Adding solutions for day 11

---
+++ src/day_11/solution_pt2.ts
@@ -0,0 +1,110 @@
+export class Coordinate {
+  row: number;
+  column: number;
+
+  constructor(row: number, column: number) {
+    this.row = row;
+    this.column = column;
+  }
+
+  public toString = (): string => {
+    return `Coordinate (row: ${this.row}, right: ${this.column})`;
+  };
+
+  public isEqual(other: Coordinate) {
+    if (this.row === other.row && this.column === other.column) {
+      return true;
+    }
+    return false;
+  }
+
+  public getManhattanDistance = (other: Coordinate): number => {
+    return (
+      Math.abs(other.column - this.column) + Math.abs(other.row - this.row)
+    );
+  };
+}
+
+export class GalaxyScaler {
+  emptyRows: Set<number>;
+  emptyCols: Set<number>;
+  scale: number;
+
+  constructor(emptyRows: Set<number>, emptyCols: Set<number>, scale: number) {
+    this.emptyRows = emptyRows;
+    this.emptyCols = emptyCols;
+    this.scale = scale;
+  }
+
+  public scaleLocation = (input: Coordinate): Coordinate => {
+    let tmpRow = input.row;
+    let tmpCol = input.column;
+
+    for (let i = 0; i < input.row; i++) {
+      if (this.emptyRows.has(i)) {
+        tmpRow = tmpRow + (this.scale - 1);
+      }
+    }
+    for (let i = 0; i < input.column; i++) {
+      if (this.emptyCols.has(i)) {
+        tmpCol = tmpCol + (this.scale - 1);
+      }
+    }
+    return new Coordinate(tmpRow, tmpCol);
+  };
+}
+
+export const solve = (input: Array<string>): number => {
+  const galaxies: Coordinate[] = [];
+  const image: string[][] = [];
+  const emptyRows: Set<number> = new Set();
+  let cols = 0;
+  for (let i = 0; i < input.length; i++) {
+    const index = input[i];
+    if (index.indexOf('#') === -1) {
+      emptyRows.add(i);
+    }
+
+    const row: string[] = [];
+    const indexArray = index.split('');
+    for (let j = 0; j < indexArray.length; j++) {
+      const cellValue = indexArray[j];
+      row.push(cellValue);
+      if (i === 0) {
+        cols++;
+      }
+      if (cellValue === '#') {
+        galaxies.push(new Coordinate(i, j));
+      }
+    }
+    image.push(row);
+  }
+
+  const emptyCols: Set<number> = new Set();
+  for (let j = 0; j < cols; j++) {
+    let isEmpty = true;
+    for (let i = 0; i < image.length; i++) {
+      if (image[i][j] === '#') {
+        isEmpty = false;
+        break;
+      }
+    }
+    if (isEmpty) {
+      emptyCols.add(j);
+    }
+  }
+
+  const scaler = new GalaxyScaler(emptyRows, emptyCols, 1000000);
+
+  let total = 0;
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      const point1 = scaler.scaleLocation(galaxies[i]);
+      const point2 = scaler.scaleLocation(galaxies[j]);
+      const distance = point1.getManhattanDistance(point2);
+      total = total + distance;
+    }
+  }
+
+  return total;
+};
---
+++ test/day_11/solution_pt1.test.ts
@@ -0,0 +1,26 @@
+import {readFileAsStringArray} from '../test_utils/read_file';
+import {solve} from '../../src/day_11/solution_pt1';
+
+describe('test solve', () => {
+  test('example input expects 374', () => {
+    const input: Array<string> = [
+      '...#......',
+      '.......#..',
+      '#.........',
+      '..........',
+      '......#...',
+      '.#........',
+      '.........#',
+      '..........',
+      '.......#..',
+      '#...#.....',
+    ];
+    expect(solve(input)).toBe(374);
+  });
+
+  test('puzzle input expects 9693756', () => {
+    const fileInput = `${__dirname}/puzzle_input.txt`;
+    const input: Array<string> = readFileAsStringArray(fileInput);
+    expect(solve(input)).toBe(9693756);
+  });
+});
