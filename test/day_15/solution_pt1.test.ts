74eae0dd506633f3e60066760b11d4201c420181
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-15 06:01:17 +0000

    Adding solution to day 15 pt 1

---
+++ test/day_15/solution_pt1.test.ts
@@ -0,0 +1,26 @@
+import {readFileAsStringArray} from '../test_utils/read_file';
+import {solve, hashValue} from '../../src/day_15/solution_pt1';
+
+describe('test hashValue with HASH', () => {
+  test('HASH expects 52', () => {
+    expect(hashValue('HASH')).toBe(52);
+  });
+  test('rn=1 expects 30', () => {
+    expect(hashValue('rn=1')).toBe(30);
+  });
+});
+
+describe('test solve', () => {
+  test('example input expects 1320', () => {
+    const input: Array<string> = [
+      'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7',
+    ];
+    expect(solve(input)).toBe(1320);
+  });
+
+  test('puzzle input expects something', () => {
+    const fileInput = `${__dirname}/puzzle_input.txt`;
+    const input: Array<string> = readFileAsStringArray(fileInput);
+    expect(solve(input)).toBe(514025);
+  });
+});
---
+++ test/day_15/solution_pt2.test.ts
@@ -0,0 +1,15 @@
+import {readFileAsStringArray} from '../test_utils/read_file';
+import {solve} from '../../src/day_15/solution_pt2';
+
+describe('test solve', () => {
+  test('example input expects 0', () => {
+    const input: Array<string> = [];
+    expect(solve(input)).toBe(0);
+  });
+
+  xtest('puzzle input expects something', () => {
+    const fileInput = `${__dirname}/puzzle_input.txt`;
+    const input: Array<string> = readFileAsStringArray(fileInput);
+    expect(solve(input)).toBe(0);
+  });
+});
